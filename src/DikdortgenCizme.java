71c802c7af7bd9c5bfa3a2ec8e6e11e5e686dbc0
ismai1kaya <ikaya9597@gmail.com>
2022-03-14 12:57:55 +0300

    Java-DikdortgenCizme

---
+++ src/DikdortgenCizme.java
@@ -0,0 +1,40 @@
+//İSMAİL KAYA 202013171071
+import java.util.Scanner;
+
+class DikdortgenOdevi{
+
+    void DiktörtgenCizim(int KKenar, int UKenar) {
+
+
+        for(int sayac=1;sayac<=KKenar;sayac++ ) {
+
+
+            for(int yildizSayaci=1;yildizSayaci<=UKenar;yildizSayaci++) {
+                System.out.print("* ");
+            }
+
+            System.out.print("\n");
+
+        }
+    }
+}
+
+
+public class DikdortgenCizme {
+
+    public static void main(String[] args) {
+
+        Scanner degerNesnesi = new Scanner(System.in);
+        DikdortgenOdevi dikdörtgen = new DikdortgenOdevi();
+        System.out.print("Üçgenin kısa kenarını giriniz:");
+        int KKenar= degerNesnesi.nextInt();
+        System.out.println("");
+        System.out.print("Üçgenin uzun kenarını giriniz:");
+        int UKenar = degerNesnesi.nextInt();
+
+        System.out.println("");
+        dikdörtgen.DiktörtgenCizim(KKenar, UKenar);
+    }
+
+}
+
